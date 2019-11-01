a5682272692682b251cf992b8c48d5e1803bb070
Javier Repetto de los Dolores <javier.repettodelosdolres@gmail.com>
2019-11-01 11:27:18 +0100

    dependencies installed!

--- app.js
+++ app.js
@@ -32,6 +32,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
