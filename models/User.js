863114b63a33265625280227f38461e2743c134d
Javier Repetto de los Dolores <javier.repettodelosdolres@gmail.com>
2019-11-01 11:58:34 +0100

    try1

---
+++ models/User.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const userSchema = new Schema({
+    name: { type: String, required: true },
+    mail: { type: String, unique: true, required: true },
+    city: { type: String },
+    phone: { type: Number },
+}, {
+    timestamps: {
+        createdAt: "createdAt", updatedAt:
+            "updatedAt"
+    }
+});
+const User = mongoose.model("User", userSchema);
+module.exports = User;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,53 @@
 var express = require('express');
 var router = express.Router();
+const User = require("../models/User");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+router.post('/signup', async (req, res, next) => {
+  console.log('estoy entrando')
+  const {
+    name,
+    mail,
+    city,
+    phone
+  } = req.body;
+  // validamos si los valores de los inputs llegan vacíos
+  if (name === "" || mail === "") {
+    res.render("index", {
+      errorMessage: "Indicate a name, mail and to register"
+    });
+    return;
+  }
+  //busco en la BD si existe el username
+  User.findOne({
+    mail: mail
+  })
+    .then(user => {
+      if (user !== null) {
+        res.render("index", {
+          errorMessage: "The mail already exists!"
+        });
+        return;
+      }
+    })
+    .catch(error => {
+      next(error);
+    });
+  const userCreated = await User.create({
+    name,
+    mail,
+    city,
+    phone
+  })
+  req.session.currentUser = userCreated
+  res.redirect("index");
+});
+
+
 
 module.exports = router;
